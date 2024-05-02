5d093dcf3959794c0a1c260c6b47274aea98052d
vy2000eng <vy2000eng@gmail.com>
2024-05-02 14:53:47 -0400

    Initial Commit

---
+++ Planify_v4/Planify_v4App.swift
@@ -0,0 +1,17 @@
+//
+//  Planify_v4App.swift
+//  Planify_v4
+//
+//  Created by VladyslavYatsuta on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Planify_v4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
