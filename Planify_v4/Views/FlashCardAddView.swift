8ad280a6a8b503ae80027d67cb77060182e8051a
vy2000eng <vy2000eng@gmail.com>
2024-05-24 09:47:54 -0400

    FCAVVM is implemented and adding flashcards works

--- Planify_v4/Views/FlashCardAddView.swift
+++ Planify_v4/Views/FlashCardAddView.swift
@@ -11,9 +11,21 @@ struct FlashCardAddView: View {
     @Environment(\.dismiss) private var dismiss
     
     
-    @State var flashcard = FlashCard()
+    @State var flashcard = FlashCard(front: "Term...           ", back: "Definition...          ")
     @State var isEditingFront = false
     @State var isEditingBack = false
+    @State var countFront = 0
+    @State var countBack = 0
+ 
+    
+    
+    enum Field: Hashable {
+          case front
+          case back
+      }
+    
+    @FocusState private var focusedField: Field?
+    
     
     var onInsert: (FlashCard) -> Void
     
@@ -32,30 +44,67 @@ struct FlashCardAddView: View {
     
     var body: some View {
         Form{
-            if !isEditingFront{
-                TextField(text:$flashcard.front, label: {
-                    Text("Term")
-                })
-                .onTapGesture{
+            VStack {
+                    if isEditingFront {
+                        Text(flashcard.front).foregroundColor(.clear).padding(8)
+                            .frame(maxWidth: .infinity)
+                            .overlay(
+                                TextEditor(text:$flashcard.front)
+                                    .focused($focusedField, equals: .front)
+
+                            )
+                            .frame(minHeight: 20.0)
+                    } else {
+                        Text(flashcard.front)
+                            .fixedSize(horizontal: false, vertical: true) // Expand vertically based on content
+                            .foregroundColor(.gray)
+                    }
+                }
+                .font(.subheadline)
+                .padding([.leading, .trailing,.top])
+                .onTapGesture {
                     isEditingFront = true
                     isEditingBack = false
+                    focusedField = .front
+                    if countFront == 0{
+                        flashcard.front = ""
+                    }
+                    countFront+=1
+
+                }.onSubmit {
+                    countFront = 0
                 }
-            }else{
-                TextEditor(text: $flashcard.front)
-            }
-            if !isEditingBack{
-                TextField(text:$flashcard.back, label: {
-                    Text("Defintion")
-                })
-                .onTapGesture {
-                
-                    isEditingBack = true
-                    isEditingFront = false
-                }
-            }else{
-                TextEditor(text: $flashcard.back)
-            }
             
+            
+            
+            VStack {
+                if isEditingBack {
+                    Text(flashcard.back).foregroundColor(.clear).padding(8)
+                        .frame(maxWidth: .infinity)
+                        .overlay(
+                            TextEditor(text:$flashcard.back)
+                                .focused($focusedField, equals: .back)
+
+                        )
+                        .frame(minHeight: 20.0)
+                } else {
+                    Text(flashcard.back)
+                        .fixedSize(horizontal: false, vertical: true) // Expand vertically based on content
+                        .foregroundColor(.gray) // Expand vertically based on content
+                }
+            }
+            .font(.subheadline)
+            .padding([.leading, .trailing,.top])
+            .onTapGesture {
+                isEditingFront = false
+                isEditingBack = true
+                focusedField = .back
+                if countBack == 0{
+                    flashcard.back = ""
+                }
+                countBack+=1
+            }
+
             Button(action: {
                 isEditingBack = false
                 isEditingFront = false
@@ -89,3 +138,18 @@ struct FlashCardAddView: View {
         
     }
 }
+
+
+//if isEditingFront {
+//                Text(flashcard.front).foregroundColor(.clear).padding(8)
+//
+//                    .frame(maxWidth: .infinity)
+//                    .overlay(
+//                        TextEditor(text: $flashcard.front)
+//
+//                    )
+//                    .frame(minHeight: 20.0)
+//            } else {
+//                Text(flashcard.front)
+//                    .fixedSize(horizontal: false, vertical: true) // Expand vertically based on content
+//            }
