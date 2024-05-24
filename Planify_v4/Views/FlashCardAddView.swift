321e0a023299e066307c54b53d667f9cd71f291c
vy2000eng <vy2000eng@gmail.com>
2024-05-24 11:11:51 -0400

    the AFCV is now working beautifully

--- Planify_v4/Views/FlashCardAddView.swift
+++ Planify_v4/Views/FlashCardAddView.swift
@@ -12,13 +12,8 @@ struct FlashCardAddView: View {
     
     
     @State var flashcard = FlashCard(front: "Term...           ", back: "Definition...          ")
-    @State var isEditingFront = false
-    @State var isEditingBack = false
-    @State var countFront = 0
-    @State var countBack = 0
- 
-    
-    
+  
+
     enum Field: Hashable {
           case front
           case back
@@ -31,83 +26,68 @@ struct FlashCardAddView: View {
     
     init(insertFlashCardCallBack: @escaping (FlashCard)->Void){
         onInsert = insertFlashCardCallBack
-        
     }
     
     func triggerInsertFlashCardCallBack(flashcard: FlashCard){
-        
         onInsert(flashcard)
-        
     }
-    
-    
     
     var body: some View {
         Form{
-            VStack {
-                    if isEditingFront {
-                        Text(flashcard.front).foregroundColor(.clear).padding(8)
-                            .frame(maxWidth: .infinity)
-                            .overlay(
-                                TextEditor(text:$flashcard.front)
-                                    .focused($focusedField, equals: .front)
-
-                            )
-                            .frame(minHeight: 20.0)
-                    } else {
-                        Text(flashcard.front)
-                            .fixedSize(horizontal: false, vertical: true) // Expand vertically based on content
-                            .foregroundColor(.gray)
+            VStack(alignment: .leading){
+                Text(flashcard.front)
+                    .foregroundColor(focusedField == .front ? .clear:.gray).padding(8)
+                    .overlay(
+                        TextEditor(text:$flashcard.front)
+                            .focused($focusedField, equals: .front)
+                            .foregroundColor(focusedField == .front ? .black:.clear)
+                    )
+                    .frame(minHeight: 20.0)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .font(.subheadline)
+                    .padding([.leading, .trailing,.top])
+                    .onTapGesture {
+                        if flashcard.front == "Term...           "{
+                            flashcard.front = ""
+                        }
+                        if flashcard.back.isEmpty{
+                            flashcard.back = "Definition...          "
+                        }
+                        focusedField = .front
                     }
-                }
-                .font(.subheadline)
-                .padding([.leading, .trailing,.top])
-                .onTapGesture {
-                    isEditingFront = true
-                    isEditingBack = false
-                    focusedField = .front
-                    if countFront == 0{
-                        flashcard.front = ""
-                    }
-                    countFront+=1
-
-                }.onSubmit {
-                    countFront = 0
-                }
-            
-            
-            
-            VStack {
-                if isEditingBack {
-                    Text(flashcard.back).foregroundColor(.clear).padding(8)
-                        .frame(maxWidth: .infinity)
+                
+                Rectangle()
+                    .frame(height: 2) // Adjust thickness here
+                    .foregroundColor(.black) // Set color to match a typical divider
+                    .opacity(0.5)
+                
+                //VStack {
+                    Text(flashcard.back)
+                        .foregroundColor(focusedField == .back ? .clear:.gray).padding(8)
                         .overlay(
                             TextEditor(text:$flashcard.back)
                                 .focused($focusedField, equals: .back)
-
+                                .foregroundColor(focusedField == .back ? .black:.clear)
                         )
                         .frame(minHeight: 20.0)
-                } else {
-                    Text(flashcard.back)
-                        .fixedSize(horizontal: false, vertical: true) // Expand vertically based on content
-                        .foregroundColor(.gray) // Expand vertically based on content
-                }
+                        .fixedSize(horizontal: false, vertical: true)
+                        .font(.subheadline)
+                        .padding([.leading, .trailing,.top])
+                        .onTapGesture {
+                            if flashcard.front.isEmpty{
+                                flashcard.front = "Term...           "
+                            }
+                            
+                            if flashcard.back == "Definition...          "{
+                                flashcard.back = ""
+                            }
+                            focusedField = .back
+                         
+                        }
+               // }
             }
-            .font(.subheadline)
-            .padding([.leading, .trailing,.top])
-            .onTapGesture {
-                isEditingFront = false
-                isEditingBack = true
-                focusedField = .back
-                if countBack == 0{
-                    flashcard.back = ""
-                }
-                countBack+=1
-            }
-
             Button(action: {
-                isEditingBack = false
-                isEditingFront = false
+              
                 triggerInsertFlashCardCallBack(flashcard: flashcard)
                 dismiss()
             },
@@ -139,17 +119,3 @@ struct FlashCardAddView: View {
     }
 }
 
-
-//if isEditingFront {
-//                Text(flashcard.front).foregroundColor(.clear).padding(8)
-//
-//                    .frame(maxWidth: .infinity)
-//                    .overlay(
-//                        TextEditor(text: $flashcard.front)
-//
-//                    )
-//                    .frame(minHeight: 20.0)
-//            } else {
-//                Text(flashcard.front)
-//                    .fixedSize(horizontal: false, vertical: true) // Expand vertically based on content
-//            }
